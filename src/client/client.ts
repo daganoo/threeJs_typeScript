f827f8e4807e742216275339daa4ff0f8c2953b2
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2024-10-23 01:19:15 +0100

    sm changes

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,13 +1,18 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
-
+scene.background = new THREE.Color(0x555555)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+// new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+//controls.addEventListener('change',render)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
@@ -18,6 +23,7 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
@@ -25,12 +31,16 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight)
   render()
 }
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
 
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  stats.update()
+  //cube.rotation.x += 0.01
+  //cube.rotation.y += 0.01
 
   render()
 }
@@ -40,3 +50,4 @@ function render() {
 }
 
 animate()
+//render()
